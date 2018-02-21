daee728ff919e68a126474ad136d859b2e96c87a
CosmosRomantica <mycatshoegazer@outlook.com>
2018-02-21 15:00:48 +0300

    VS deploy.

---
+++ Surface.NET/Classic desktop/Program.cs
@@ -0,0 +1,23 @@
+﻿// Surface.NET/Surface.NET Classic/Program.cs
+// 21.02.2018 14:42
+// Cosmos Romantica (Александр Гелета) <mycatshoegazer@outlook.com>
+
+using System;
+using System.Windows.Forms;
+
+namespace SurfaceClassic
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MDIMainForm());
+        }
+    }
+}

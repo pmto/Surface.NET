1a84411ca50442a4d0a56a760c7973e391f41197
CosmosRomantica <mycatshoegazer@outlook.com>
2018-02-21 17:09:48 +0300

    Created license header.

--- Surface.NET/Classic desktop/Program.cs
+++ Surface.NET/Classic desktop/Program.cs
@@ -1,6 +1,22 @@
-﻿// Surface.NET/Surface.NET Classic/Program.cs
-// 21.02.2018 14:42
-// Cosmos Romantica (Александр Гелета) <mycatshoegazer@outlook.com>
+﻿// Surface.NET, Surface.NET Classic
+// Copyright (C) 2018  MyCatShoegazer
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// FILE: Surface.NET/Surface.NET Classic/Program.cs
+// MODIFIED: 21.02.2018 14:42
+// DEVELOPER: MyCatShoegazer <mycatshoegazer@outlook.com>
 
 using System;
 using System.Windows.Forms;

cbeab07ebbe85f12395771506c7033701aaa3bff
CosmosRomantica <mycatshoegazer@outlook.com>
2018-02-25 22:26:31 +0300

    Implemented override tests for Point3D.

---
+++ Surface.NET/Surface.NET Library Tests/Point3D/Point3DOverridesTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Surface.NET_Library_Tests.Point3D {
+    [TestCategory("Point3D Overrides")]
+    [TestClass]
+    public class Point3DOverridesTests {
+        private SurfaceLib.Types.Point3D _point;
+
+        [TestCategory("Point3D Overrides")]
+        [TestInitialize]
+        public void Initialize() {
+            this._point = new SurfaceLib.Types.Point3D(3.2, 5.1, 4);
+        }
+
+        [TestCategory("Point3D Overrides")]
+        [TestMethod]
+        public void GetHashCodeTest() {
+            var expected =
+                this._point.XAxis.GetHashCode() ^
+                this._point.YAxis.GetHashCode() ^
+                this._point.ZAxis.GetHashCode();
+            var actual = this._point.GetHashCode();
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestCategory("Point3D Overrides")]
+        [TestMethod]
+        public void EqualsTest() {
+            var temp = new SurfaceLib.Types.Point3D(2, 4.1, 5);
+            var actual = this._point.Equals(temp);
+
+            Assert.IsFalse(actual);
+        }
+
+        [TestCategory("Point3D Overrides")]
+        [TestMethod]
+        public void ToStringTest() {
+            var expected = $"\"{this._point.XAxis}\",\"{this._point.YAxis}\",\"{this._point.ZAxis}\"";
+            var actual = this._point.ToString();
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

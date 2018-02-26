c0e2516f0b403ad34f991f2fa0cfac912b6c4af8
CosmosRomantica <mycatshoegazer@outlook.com>
2018-02-26 11:54:57 +0300

    Implemented attribute marker for points.

---
+++ Surface.NET/SurfaceLib/Types/Abstract/PointAttribute.cs
@@ -0,0 +1,26 @@
+﻿// Surface.NET, Surface.NET Library
+// Copyright (C) 2018  MyCatShoegazer
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// 
+// FILE: Surface.NET/Surface.NET Library/PointAttribute.cs
+// MODIFIED: 26.02.2018 11:51
+// DEVELOPER: MyCatShoegazer <mycatshoegazer@outlook.com>
+
+using System;
+
+namespace SurfaceLib.Types.Abstract {
+    [AttributeUsage(AttributeTargets.Class)]
+    public class PointAttribute : Attribute { }
+}

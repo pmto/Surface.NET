09656561611354fe09606f5f973b45dc6849378f
MyCatShoegazer <mycatshoegazer@outlook.com>
2018-03-12 14:05:53 +0300

    Implemented settings on change saving handling.

--- Surface.NET/Surface.NET Desktop/App.xaml.cs
+++ Surface.NET/Surface.NET Desktop/App.xaml.cs
@@ -1,30 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
 using SurfaceDesktop.Properties;
 using SurfaceDesktop.Resources.Localization;
 
-namespace Surface.NET_Desktop
-{
+namespace SurfaceDesktop {
     /// <summary>
-    /// Логика взаимодействия для App.xaml
+    ///     Application entry point.
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
+        /// <summary>
+        ///     Rises when application doing exit.
+        /// </summary>
         private void App_OnExit(object sender, ExitEventArgs e) {
+            SaveSettings();
+        }
+
+        /// <summary>
+        ///     Save settings and avoid application crush on errors.
+        /// </summary>
+        private static void SaveSettings() {
+            /*
+             * Here we trying to save application changes
+             * and if it goes to error then showing WinForms message to user.
+             */
             try {
+                // Saving application settings
                 Settings.Default.Save();
             }
             catch (Exception) {
+                // Show message on errors
                 MessageBox.Show(LocalizedStrings.ApplicationSaveSettingsErrorMessageText,
                     LocalizedStrings.ApplicationSaveSettingsErrorMessageTitle, MessageBoxButton.OK,
                     MessageBoxImage.Error);
             }
+        }
+
+        /// <summary>
+        ///     Rises when application starting.
+        /// </summary>
+        private void App_OnStartup(object sender, StartupEventArgs e) {
+            /*
+             * Here on application startup we registering event handler
+             * which rises when settings changed.
+             */
+            Settings.Default.PropertyChanged += (o, args) => {
+                // Saving changes
+                SaveSettings();
+            };
         }
     }
 }
--- Surface.NET/Surface.NET Desktop/Properties/AssemblyInfo.cs
+++ Surface.NET/Surface.NET Desktop/Properties/AssemblyInfo.cs
@@ -51,9 +51,9 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.3.51.1046")]
-[assembly: AssemblyFileVersion("2018.3.49.1046")]
+[assembly: AssemblyVersion("2018.3.53.1102")]
+[assembly: AssemblyFileVersion("2018.3.51.1102")]
 
-[assembly: AssemblyInformationalVersion("2018.3.50")]
+[assembly: AssemblyInformationalVersion("2018.3.52")]
 [assembly: NeutralResourcesLanguage("en-US")]
 

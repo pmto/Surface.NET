88e7977d5bd3889a4142d2034310bb053641bfa0
MyCatShoegazer <mycatshoegazer@outlook.com>
2018-03-12 13:36:32 +0300

    Loaded translations.

--- Surface.NET/Surface.NET Desktop/Properties/AssemblyInfo.cs
+++ Surface.NET/Surface.NET Desktop/Properties/AssemblyInfo.cs
@@ -51,9 +51,9 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.3.49.1023")]
-[assembly: AssemblyFileVersion("2018.3.47.1023")]
+[assembly: AssemblyVersion("2018.3.50.1036")]
+[assembly: AssemblyFileVersion("2018.3.48.1036")]
 
-[assembly: AssemblyInformationalVersion("2018.3.48")]
+[assembly: AssemblyInformationalVersion("2018.3.49")]
 [assembly: NeutralResourcesLanguage("en-US")]
 

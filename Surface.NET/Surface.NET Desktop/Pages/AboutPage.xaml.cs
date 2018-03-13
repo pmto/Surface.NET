93d36b1329edfd7d75ace3b6cc1b813fe3262db7
MyCatShoegazer <mycatshoegazer@outlook.com>
2018-03-13 15:01:28 +0300

    Implemented loading assembly info to page controls.

--- Surface.NET/Surface.NET Desktop/Pages/AboutPage.xaml.cs
+++ Surface.NET/Surface.NET Desktop/Pages/AboutPage.xaml.cs
@@ -1,28 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Reflection;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace SurfaceDesktop.Pages
-{
+namespace SurfaceDesktop.Pages {
     /// <summary>
-    /// Логика взаимодействия для AboutPage.xaml
+    ///     <see cref="AboutPage"/> logic.
     /// </summary>
-    public partial class AboutPage : Page
-    {
-        public AboutPage()
-        {
+    public partial class AboutPage : Page {
+        public AboutPage() {
+            // Initialize page components
             this.InitializeComponent();
+            
+            // Load assembly info to controls
+            this.LoadAssemblyInfo();
+        }
+
+        /// <summary>
+        ///     Loads assembly info to page controls of current application
+        ///     using reflection mechanism.
+        /// </summary>
+        private void LoadAssemblyInfo() {
+            var application = Assembly.GetExecutingAssembly();
+
+            this.TitleTextBox.Text =
+                ((AssemblyTitleAttribute) application.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0])
+                .Title;
+            this.CompanyTextBox.Text =
+                ((AssemblyCompanyAttribute) application.GetCustomAttributes(typeof(AssemblyCompanyAttribute), false)[0])
+                .Company;
+            this.ProductTextBox.Text =
+                ((AssemblyProductAttribute) application.GetCustomAttributes(typeof(AssemblyProductAttribute), false)[0])
+                .Product;
+            this.CopyrightTextBox.Text =
+                ((AssemblyCopyrightAttribute) application.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)
+                    [0])
+                .Copyright;
+            this.TrademarkTextBox.Text =
+                ((AssemblyTrademarkAttribute) application.GetCustomAttributes(typeof(AssemblyTrademarkAttribute), false)
+                    [0])
+                .Trademark;
+            this.VersionTextBox.Text =
+                ((AssemblyVersionAttribute) application.GetCustomAttributes(typeof(AssemblyVersionAttribute), false)[0])
+                .Version;
         }
     }
 }

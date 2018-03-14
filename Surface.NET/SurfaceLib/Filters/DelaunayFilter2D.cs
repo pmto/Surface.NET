b3978d0370b3c0c7b6d70b13b633a87670134b34
MyCatShoegazer <mycatshoegazer@outlook.com>
2018-03-14 19:29:25 +0300

    Refactored.

--- Surface.NET/SurfaceLib/Filters/DelaunayFilter2D.cs
+++ Surface.NET/SurfaceLib/Filters/DelaunayFilter2D.cs
@@ -20,84 +20,94 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using SurfaceLib.Types;
 
 namespace SurfaceLib.Filters {
-    /// <summary>A 2d Delaunay triangulation class.</summary>
-    /// <remarks>The triangulation doesn't support multiple points with identical x and y coordinates,
-    /// nor does it support dublicate points.
-    /// Vertex-lists with duplicate points may result in strange triangulations with intersecting edges or
-    /// may cause the algorithm to fail. 
-    /// Uses a simple O(n**2) algorithm based on Paul Bourke's "An Algorithm for Interpolating 
-    /// Irregularly-Spaced Data with Applications in Terrain Modelling". 
-    /// Uses an enhanced incircle-predicate for counterclockwise orientated triangles.</remarks>
-    public class DelaunayFilter2D
-    {
-        /// <summary>Performs the 2d Delaunay triangulation on a set of n vertices in O(n**2) time.</summary>
-        /// <param name="triangulationPoints">The points to triangulate.</param>
-        /// <returns>A list of Delaunay-triangles.</returns>
-        public List<Triangle> Triangulate(List<Point3D> triangulationPoints)
-        {
-            if (triangulationPoints.Count < 3) throw new ArgumentException("Can not triangulate less than three vertices!");
+    /// <summary>
+    ///     A 2D Delaunay triangulation class.
+    /// </summary>
+    /// <remarks>
+    ///     The triangulation doesn't support multiple points with identical x and y coordinates,
+    ///     nor does it support dublicate points.
+    ///     Vertex-lists with duplicate points may result in strange triangulations with intersecting edges or
+    ///     may cause the algorithm to fail.
+    ///     Uses a simple O(n**2) algorithm based on Paul Bourke's "An Algorithm for Interpolating
+    ///     Irregularly-Spaced Data with Applications in Terrain Modelling".
+    ///     Uses an enhanced incircle-predicate for counterclockwise orientated triangles.
+    /// </remarks>
+    public class DelaunayFilter2D {
+        /// <summary>
+        ///     Performs the 2D Delaunay triangulation on a set of n vertices in O(n**2) time.
+        /// </summary>
+        /// <param name="triangulationPoints">
+        ///     The points to triangulate.
+        /// </param>
+        /// <returns>
+        ///     A list of Delaunay-triangles.
+        /// </returns>
+        public IList<Triangle> Triangulate(IList<Point3D> triangulationPoints) {
+            if (triangulationPoints.Count < 3)
+                throw new ArgumentException("Can not triangulate less than three vertices!");
 
             // The triangle list
-            List<Triangle> triangles = new List<Triangle>(); ;
+            var triangles = new List<Triangle>();
 
-            // The "supertriangle" which encompasses all triangulation points.
-            // This triangle initializes the algorithm and will be removed later.
-            Triangle superTriangle = this.SuperTriangle(triangulationPoints);
+            /*
+             * The "supertriangle" which encompasses all triangulation points.
+             * This triangle initializes the algorithm and will be removed later.
+             */
+            var superTriangle = SuperTriangle(triangulationPoints);
             triangles.Add(superTriangle);
 
-            // Include each point one at a time into the existing triangulation
-            for (int i = 0; i < triangulationPoints.Count; i++)
-            {
-                // Initialize the edge buffer.
-                List<Edge> EdgeBuffer = new List<Edge>();
+            /*
+             * Include each point one at a time
+             * into the existing triangulation.
+             */
+            foreach (var triangulationPoint in triangulationPoints) {
+                // Initialize the edge buffer
+                var edgeBuffer = new List<Edge>();
 
-                // If the actual vertex lies inside the circumcircle, then the three edges of the 
-                // triangle are added to the edge buffer and the triangle is removed from list.                             
-                for (int j = triangles.Count - 1; j >= 0; j--)
-                {
-                    Triangle t = triangles[j];
-                    if (t.ContainsInCircumcircle(triangulationPoints[i]) > 0)
-                    {
-                        EdgeBuffer.Add(new Edge(t.Vertex1, t.Vertex2));
-                        EdgeBuffer.Add(new Edge(t.Vertex2, t.Vertex3));
-                        EdgeBuffer.Add(new Edge(t.Vertex3, t.Vertex1));
-                        triangles.RemoveAt(j);
-                    }
+                /*
+                 * If the actual vertex lies inside the circumcircle, then the three edges of the 
+                 * triangle are added to the edge buffer and the triangle is removed from list.
+                 */
+                for (var j = triangles.Count - 1; j >= 0; j--) {
+                    var triangle = triangles[j];
+                    if (!(triangle.ContainsInCircumcircle(triangulationPoint) > 0)) continue;
+                    edgeBuffer.Add(new Edge(triangle.Vertex1, triangle.Vertex2));
+                    edgeBuffer.Add(new Edge(triangle.Vertex2, triangle.Vertex3));
+                    edgeBuffer.Add(new Edge(triangle.Vertex3, triangle.Vertex1));
+                    triangles.RemoveAt(j);
                 }
 
-                // Remove duplicate edges. This leaves the convex hull of the edges.
-                // The edges in this convex hull are oriented counterclockwise!
-                for (int j = EdgeBuffer.Count - 2; j >= 0; j--)
-                {
-                    for (int k = EdgeBuffer.Count - 1; k >= j + 1; k--)
-                    {
-                        if (EdgeBuffer[j] == EdgeBuffer[k])
-                        {
-                            EdgeBuffer.RemoveAt(k);
-                            EdgeBuffer.RemoveAt(j);
-                            k--;
-                            continue;
-                        }
-                    }
+                /*
+                 * Remove duplicate edges. This leaves the convex hull of the edges.
+                 * The edges in this convex hull are oriented counterclockwise!
+                 */
+                for (var j = edgeBuffer.Count - 2; j >= 0; j--)
+                for (var k = edgeBuffer.Count - 1; k >= j + 1; k--) {
+                    if (edgeBuffer[j] != edgeBuffer[k]) continue;
+                    edgeBuffer.RemoveAt(k);
+                    edgeBuffer.RemoveAt(j);
+                    k--;
                 }
 
-                // Generate new counterclockwise oriented triangles filling the "hole" in
-                // the existing triangulation. These triangles all share the actual vertex.
-                for (int j = 0; j < EdgeBuffer.Count; j++)
-                {
-                    triangles.Add(new Triangle(EdgeBuffer[j].StartPoint3D, EdgeBuffer[j].EndPoint3D, triangulationPoints[i]));
-                }
+                /*
+                 * Generate new counterclockwise oriented triangles filling the "hole" in the existing triangulation.
+                 * These triangles all share the actual vertex.
+                 */
+                triangles.AddRange(
+                    edgeBuffer.Select(t => new Triangle(t.StartPoint3D, t.EndPoint3D, triangulationPoint)));
             }
 
-            // We don't want the supertriangle in the triangulation, so
-            // remove all triangles sharing a vertex with the supertriangle.
-            for (int i = triangles.Count - 1; i >= 0; i--)
-            {
-                if (triangles[i].SharesVertexWith(superTriangle)) triangles.RemoveAt(i);
-            }
+            /*
+             * We don't want the supertriangle in the triangulation,
+             * so remove all triangles sharing a vertex with the supertriangle.
+             */
+            for (var i = triangles.Count - 1; i >= 0; i--)
+                if (triangles[i].SharesVertexWith(superTriangle))
+                    triangles.RemoveAt(i);
 
             // Return the triangles
             return triangles;
@@ -112,20 +122,18 @@ namespace SurfaceLib.Filters {
         /// <returns>
         ///     Returns a triangle that encompasses all triangulation points.
         /// </returns>
-        private Triangle SuperTriangle(List<Point3D> triangulationPoints)
-        {
+        private static Triangle SuperTriangle(IList<Point3D> triangulationPoints) {
             var m = triangulationPoints[0].XAxis;
 
-            // get the extremal x and y coordinates
-            for (var i = 1; i < triangulationPoints.Count; i++)
-            {
+            // Get the extremal x and y coordinates
+            for (var i = 1; i < triangulationPoints.Count; i++) {
                 var xAbs = Math.Abs(triangulationPoints[i].XAxis);
                 var yAbs = Math.Abs(triangulationPoints[i].YAxis);
                 if (xAbs > m) m = xAbs;
                 if (yAbs > m) m = yAbs;
             }
 
-            // make a triangle
+            // Make a triangle
             var sp1 = new Point3D(10 * m, 0, 0);
             var sp2 = new Point3D(0, 10 * m, 0);
             var sp3 = new Point3D(-10 * m, -10 * m, 0);

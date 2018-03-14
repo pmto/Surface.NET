bb99ef1202e37e7ac81aa0a7f8edefe1c7308928
MyCatShoegazer <mycatshoegazer@outlook.com>
2018-03-14 20:20:00 +0300

    Implemented showing progress ring on frame loading.

--- Surface.NET/Surface.NET Desktop/Properties/AssemblyInfo.cs
+++ Surface.NET/Surface.NET Desktop/Properties/AssemblyInfo.cs
@@ -51,9 +51,9 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2018.3.109.1243")]
-[assembly: AssemblyFileVersion("2018.3.107.1243")]
+[assembly: AssemblyVersion("2018.3.110.1715")]
+[assembly: AssemblyFileVersion("2018.3.108.1715")]
 
-[assembly: AssemblyInformationalVersion("2018.3.106")]
+[assembly: AssemblyInformationalVersion("2018.3.107")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
